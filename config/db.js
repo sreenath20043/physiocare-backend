dfe1941cb3f161953c0340b5c470dee3796d1f5d
sreenath20043 <sreenathsreekumar2004@gmail.com>
2026-03-12 11:21:00 +0530

    deploy.4 commit

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 require('dotenv').config()
 
 const mongoose = require('mongoose')
+
 const dbString = process.env.connectionString
 
 mongoose.connect(dbString).then(()=>{
-    console.log('Connected to MongoDB');
-}).catch((err)=>{
-    console.log('Error connecting to MongoDB',err);
-    
+    console.log('Connected to MongoDB')
 })
-
-
+.catch((err)=>{
+    console.log('Error connecting to MongoDB', err)
+})
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //1 import express
 const express = require('express')
-//6 import db connection
-const db = require('./config/db')
 
-//7 import cros
+//6 import db connection
+require('./config/db')
+
+//7 import cors
 const cors = require('cors')
 
 //8 import routes
 const router = require('./router/route')
-// const appMiddleware = require('./middlewares/appMiddleware')
 
 //2 app creation
 const physiocareServer = express()
@@ -19,20 +19,16 @@ physiocareServer.use(cors())
 //10 use json middleware
 physiocareServer.use(express.json())
 
-//12implementin middlewares
-// physiocareServer.use(appMiddleware)
-
 //11use route
 physiocareServer.use(router)
 
 //multer
 physiocareServer.use('/uploads',express.static('./uploads'))
 
-//3 poert define
-const PORT = 3000;
+//3 port define
+const PORT = process.env.PORT || 3000
 
 //4 server start
 physiocareServer.listen(PORT,()=>{
-    console.log(`Physiocare server started on port ${PORT}`);
-    
+    console.log(`Physiocare server started on port ${PORT}`)
 })
